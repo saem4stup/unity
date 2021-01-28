230122acd91bd7505a359d0b511a0393622ade7d
JinHoLeeee <55572533+JinHoLeeee@users.noreply.github.com>
2021-01-28 17:02:42 +0900

    Logo and title

---
+++ AnchorTest1/Assets/Scripts/AnchorScript.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA;
+using UnityEngine.XR.WSA.Persistence;
+
+public class AnchorScript : MonoBehaviour
+{
+    public GameObject rootGameObject;
+    WorldAnchorStore worldAnchorStore = null;
+    bool savedRoot = false;
+    string[] ids = null;
+    bool storeLoaded = false;
+    WorldAnchor anchor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        WorldAnchorStore.GetAsync(StoreLoaded);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (storeLoaded)
+        {
+            EnumerateIDS();
+        }
+    }
+
+    void EnumerateIDS()
+    {
+        Debug.Log("Enumerating IDS");
+        storeLoaded = false;
+        if (worldAnchorStore != null)
+        {
+            ids = this.worldAnchorStore.GetAllIds();
+            Debug.Log("id length of anchors: " + ids.Length);
+            for(int index = 0; index < ids.Length; index++)
+            {
+                Debug.Log(ids[index]);
+            }
+            LoadAnchor();
+        }
+        else
+        {
+            Debug.Log("Enumerating IDs: worldanchorstore null");
+        }
+    }
+
+    private void StoreLoaded(WorldAnchorStore store)
+    {
+        this.worldAnchorStore = store;
+        Debug.Log("StoreLoaded: World anchor store loaded successfully");
+        storeLoaded = true;
+    }
+
+    private void LoadAnchor()
+    {
+        this.savedRoot = this.worldAnchorStore.Load("Root", rootGameObject);
+        if (!this.savedRoot)
+        {
+            Debug.Log("Loadgame: no rootobject anchor saved previously");
+        }
+
+    }
+
+    public void SaveAnchor()
+    {
+        anchor = rootGameObject.AddComponent<WorldAnchor>();
+        //save data about holograms positioned by this world anchor
+        if (!this.savedRoot && anchor != null)//Save the root only once
+        {
+            this.savedRoot = this.worldAnchorStore.Save("root", anchor);
+            Debug.Log("Saved anchor: " + this.savedRoot);
+        }
+        else
+        {
+            Debug.Log("Root has already been saved");
+        }
+    }
+
+    public void DestroyExistingAnchor()
+    {
+        anchor = rootGameObject.GetComponent<WorldAnchor>();
+        if (anchor != null)
+        {
+            Destroy(anchor);
+            worldAnchorStore.Delete("root");
+            this.savedRoot = false;
+        }
+    }
+}
---
+++ AnchorTest1/Assets/Scripts/DebugText.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DebugText : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshPro textMeshPro = default;
+    void Start()
+    {
+        textMeshPro.text = "test test";
+        Application.logMessageReceived += HandleLog;
+    }
+
+    void HandleLog(string logString, string stackTrace, LogType type)
+    {
+        textMeshPro.text += logString + "\n";
+    }
+}
